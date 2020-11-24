e55d9e0b0ee04c5210511cee42eecef173623117
andrejusnec <73877307+andrejusnec@users.noreply.github.com>
2020-11-24 20:24:19 +0200

    socialrender part1

---
+++ js/data/socialData.js
@@ -0,0 +1,32 @@
+const socialData = [
+    {
+        icon: 'facebook',
+        link: '#'
+    },
+    {
+        icon: 'twitter',
+        link: '#'
+    },
+    {
+        icon: 'dribbble',
+        link: '#'
+    },
+    {
+        icon: 'tumblr',
+        link: '#'
+    },
+    {
+        icon: 'instagram',
+        link: '#'
+    },
+    {
+        icon: 'linkedin',
+        link: '#'
+    },
+    {
+        icon: 'rss',
+        link: '#'
+    },
+];
+
+export { socialData }
