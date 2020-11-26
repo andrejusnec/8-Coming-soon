778dfe126302bd6a696afa042a756fda51e6f758
andrejusnec <73877307+andrejusnec@users.noreply.github.com>
2020-11-26 18:27:49 +0200

    RenderClock

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { renderSocials } from "./components/social/renderSocials.js";
 import { socialsData } from './data/socialsData.js';
+import { renderClock } from './components/social/clock/renderClock.js'
 
 renderSocials('footer > .row', socialsData);
 
+renderClock('.clock');
