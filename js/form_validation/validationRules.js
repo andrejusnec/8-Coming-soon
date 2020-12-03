f0ebeb044097d789134603386bf6142588b9df67
andrejusnec <73877307+andrejusnec@users.noreply.github.com>
2020-12-03 11:00:38 +0200

    Toast

--- js/form_validation/validationRules.js
+++ js/form_validation/validationRules.js
@@ -2,4 +2,12 @@ import { isValidName } from './isValidName.js';
 import { isValidEmail } from './isValidEmail.js';
 import { isValidText } from './isValidText.js';
 
-export { isValidName, isValidEmail, isValidText }
+// Validation objekto key (raktazodziai) specialiai sutampa 
+// su terminais naudojamais HTML formose esanciuose data-validation reiksmese
+const validation = {
+    email: isValidEmail,
+    text: isValidText,
+    name: isValidName
+}
+
+export { validation }
--- js/main.js
+++ js/main.js
@@ -6,11 +6,18 @@ import { progressBarData } from './data/progressBarData.js';
 import { renderAllProgressBars } from './progress-bar/renderAllProgressBars.js';
 import { formValidator } from '../js/form_validation/formValidator.js';
 
+import { Toast } from './toast/Toast.js';
+
 renderSocials('footer > .row', socialsData);
 
 renderClock('.clock');
 
 renderAllProgressBars(progressBarData);
+
+const toast = new Toast();
+toast.render();
+toast.show('success', 'Buvo gera diena');
+/*toast.hide('error', 'Cia yra klaida');*/
 
 formValidator('.hero .form');
 formValidator('main .form');
